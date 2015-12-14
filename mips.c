864977bf46a4298ed9fdfb32653b78517664e5c2
Fabricio Santolin da Silva <fabricio.tomohare@gmail.com>
2015-12-14 22:52:41 +0100

    Updating mips to work with strings and floats

--- mips.c
+++ mips.c
@@ -3,46 +3,109 @@
 int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 	fprintf(output, "\t.data\n");
 	while(symbol_table != NULL){
-		if(symbol_table->isConstant)
-			fprintf(output, "%s:\t.word %d\n", symbol_table->id, symbol_table->value.int_v);
+		if(symbol_table->isConstant || 1){
+			switch(symbol_table->type){
+				case INT_:
+					fprintf(output, "%s:\t.word %d\n",
+						symbol_table->id, symbol_table->value.int_v);
+				break;
+				case FLOAT_:
+					fprintf(output, "%s:\t.float %f\n",
+						symbol_table->id, symbol_table->value.float_v);
+				break;
+				case STRING_:
+					fprintf(output, "%s:\t.asciiz \"%s\"\n",
+						symbol_table->id, symbol_table->value.string_v);
+				break;
+				default:break;
+			}
+		}
 		symbol_table = symbol_table->next;
 	}
-	fprintf(output, "\t.globl main\n");
+	fprintf(output, "\t.text\n\t.globl main\n");
 
-	fprintf(output,"\t.text\nmain: ");
+	fprintf(output,"main: ");
 	while(code!=NULL)
 	{
-		if(code->label > 0)
+		if(code->label > 0 && code->op != NOP_)
 			fprintf(output, "next%d:",code->label);
 		switch(code->op)
 		{
 			case ADD_:
 				fprintf(stdout, "PLUS %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
-				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
-				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\taddu $t2, $t0, $t1\n");
-				fprintf(output, "\tsw $t2, %s\n",code->res->id);
+				switch(code->arg1->type)
+				{
+					case INT_:
+					fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
+					fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
+					fprintf(output, "\taddu $t2, $t0, $t1\n");
+					fprintf(output, "\tsw $t2, %s\n",code->res->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tadd.s $f2, $f0, $f1\n");
+					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
+					break;
+					default:break;
+				}
 			break;
 			case MIN_:
 				fprintf(stdout, "MIN %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
-				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
-				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\tsubu $t2, $t0, $t1\n");
-				fprintf(output, "\tsw $t2, %s\n",code->res->id);
+				switch(code->arg1->type)
+				{
+					case INT_:
+					fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
+					fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
+					fprintf(output, "\tsubu $t2, $t0, $t1\n");
+					fprintf(output, "\tsw $t2, %s\n",code->res->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tsub.s $f2, $f0, $f1\n");
+					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
+					break;
+					default:break;
+				}
 			break;
 			case MUL_:
 				fprintf(stdout, "MUL %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
-				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
-				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\tmul $t2, $t0, $t1\n");
-				fprintf(output, "\tsw $t2, %s\n",code->res->id);
+				switch(code->arg1->type)
+				{
+					case INT_:
+					fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
+					fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
+					fprintf(output, "\tmul $t2, $t0, $t1\n");
+					fprintf(output, "\tsw $t2, %s\n",code->res->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tmul.s $f2, $f0, $f1\n");
+					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
+					break;
+					default:break;
+				}
 			break;
 			case DIV_:
 				fprintf(stdout, "DIV %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
-				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
-				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\tdiv $t2, $t0, $t1\n");
-				fprintf(output, "\tsw $t2, %s\n",code->res->id);
+				switch(code->arg1->type)
+				{
+					case INT_:
+					fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
+					fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
+					fprintf(output, "\tdiv $t2, $t0, $t1\n");
+					fprintf(output, "\tsw $t2, %s\n",code->res->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tdiv.s $f2, $f0, $f1\n");
+					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
+					break;
+					default:break;
+				}
 			break;
 			case SUP_:
 				fprintf(stdout, "SUPERIOR %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
@@ -80,8 +143,18 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 			break;
 			case ASSIGN_:
 				fprintf(stdout, "ASSIGN %s %s\n",code->arg1->id, code->res->id);
-				fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
-				fprintf(output, "\tsw $t0, %s\n", code->res->id);
+				switch(code->arg1->type)
+				{
+					case INT_:
+					fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
+					fprintf(output, "\tsw $t0, %s\n", code->res->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli.s $f0, %f\n", code->arg1->value.float_v);
+					fprintf(output, "\ts.s $f0, %s\n", code->res->id);
+					break;
+					default:break;
+				}
 			break;
 			case GOTO_:
 				{
@@ -90,9 +163,31 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tj next%d\n",code->next->label);
 				}
 			break;
-			case NOP_:
+			/*case NOP_:
 				fprintf(output, "\t#NOP\n");
 				fprintf(output, "\tnop\n");
+			break;*/
+			case PRINTF_:
+			case PRINT_:
+				switch(code->arg1->type){
+					case INT_:
+					fprintf(output, "\tli $v0, 1\n");
+					fprintf(output, "\tla $a0, %s\n",code->arg1->id);
+					break;
+					case FLOAT_:
+					fprintf(output, "\tli $v0, 2\n");
+					fprintf(output, "\tla $f12, %s\n",code->arg1->id);
+					break;
+					case STRING_:
+					fprintf(output, "\tli $v0, 4\n");
+					fprintf(output, "\tla $a0, %s\n",code->arg1->id);
+					break;
+					case NULL_:
+					fprintf(output, "\tli $v0, 4\n");
+					fprintf(output, "\tla $a0, \"null\"\n");
+					break;
+				}
+				fprintf(output, "\tsyscall\n");
 			break;
 		}
 		code = code->next;
