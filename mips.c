1276dd93d2374da3f325d9726b6e57780192c536
Fabricio Santolin da Silva <fabricio.tomohare@gmail.com>
2015-12-16 00:30:20 +0100

    Up until the test file 10_if_else.c should work.

--- mips.c
+++ mips.c
@@ -27,7 +27,7 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 	fprintf(output,"main: ");
 	while(code!=NULL)
 	{
-		if(code->label > 0 && code->op != NOP_)
+		if(code->label > 0)
 			fprintf(output, "next%d:",code->label);
 		switch(code->op)
 		{
@@ -42,8 +42,8 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tsw $t2, %s\n",code->res->id);
 					break;
 					case FLOAT_:
-					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
-					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tl.s $f0, %s\n",code->arg1->id);
+					fprintf(output, "\tl.s $f1, %s\n",code->arg2->id);
 					fprintf(output, "\tadd.s $f2, $f0, $f1\n");
 					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
 					break;
@@ -61,8 +61,8 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tsw $t2, %s\n",code->res->id);
 					break;
 					case FLOAT_:
-					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
-					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tl.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tl.s $f1, %f\n",code->arg2->value.float_v);
 					fprintf(output, "\tsub.s $f2, $f0, $f1\n");
 					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
 					break;
@@ -80,8 +80,8 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tsw $t2, %s\n",code->res->id);
 					break;
 					case FLOAT_:
-					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
-					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tl.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tl.s $f1, %f\n",code->arg2->value.float_v);
 					fprintf(output, "\tmul.s $f2, $f0, $f1\n");
 					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
 					break;
@@ -99,8 +99,8 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tsw $t2, %s\n",code->res->id);
 					break;
 					case FLOAT_:
-					fprintf(output, "\tli.s $f0, %f\n",code->arg1->value.float_v);
-					fprintf(output, "\tli.s $f1, %f\n",code->arg2->value.float_v);
+					fprintf(output, "\tl.s $f0, %f\n",code->arg1->value.float_v);
+					fprintf(output, "\tl.s $f1, %f\n",code->arg2->value.float_v);
 					fprintf(output, "\tdiv.s $f2, $f0, $f1\n");
 					fprintf(output, "\ts.s $f2, %s\n",code->res->id);
 					break;
@@ -111,35 +111,41 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 				fprintf(stdout, "SUPERIOR %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
 				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
 				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\tsgt $t8, $t0, $t1\n");
-				fprintf(output, "\tbne $t8, 1, next%d\n",code->next->label);
+				//fprintf(output, "\tsgt $t8, $t0, $t1\n");
+				fprintf(output, "\tbgt $t0, $t1, next%d\n",code->res->value.int_v);
+				fprintf(output, "\tble $t0, $t1, next%d\n",code->next->res->value.int_v);
+				//fprintf(output, "\tble $t0, $t1, next%d\n",code->next->next->label);
 			break;
 			case INF_:
 				fprintf(stdout, "INF %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
 				fprintf(output, "\tlw $t0, %s\n",code->arg1->id);
 				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
-				fprintf(output, "\tslt $t8, $t0, $t1\n");
-				fprintf(output, "\tbne $t8, 1, next%d\n",code->next->label);
+				//fprintf(output, "\tslt $t8, $t0, $t1\n");
+				fprintf(output, "\tblt $t0, $t1, next%d\n",code->res->value.int_v);
+				fprintf(output, "\tbge $t0, $t1, next%d\n",code->next->res->value.int_v);
 			break;
 			case SUPEQ_:
 				fprintf(stdout, "SUPEQ %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
 				fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
 				fprintf(output, "\tlw $t1, %s\n",code->arg2->id);
 				fprintf(output, "\tsge $t8, $t0, $t1\n");
-				fprintf(output, "\tbne $t8, 1, next%d\n", code->next->label);
+				fprintf(output, "\tbge $t0, $t1, next%d\n",code->res->value.int_v);
+				fprintf(output, "\tblt $t0, $t1, next%d\n",code->next->res->value.int_v);
 			break;
 			case INFEQ_:
 				fprintf(stdout, "INFEQ %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
 				fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
 				fprintf(output, "\tlw $t1, %s\n", code->arg2->id);
 				fprintf(output, "\tsle $t8, $t0, $t1\n");
-				fprintf(output, "\tbne $t8, 1, next%d\n", code->next->label);
+				fprintf(output, "\tble $t0, $t1, next%d\n",code->res->value.int_v);
+				fprintf(output, "\tbgt $t0, $t1, next%d\n",code->next->res->value.int_v);
 			break;
 			case EQUAL_:
 				fprintf(stdout, "EQUAL %s %s %s\n",code->arg1->id, code->arg2->id , code->res->id);
 				fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
 				fprintf(output, "\tlw $t1, %s\n", code->arg2->id);
-				fprintf(output, "\tbne $t8, $t1, next%d\n", code->next->label);
+				fprintf(output, "\tbeq $t0, $t1, next%d\n",code->res->value.int_v);
+				fprintf(output, "\tbne $t0, $t1, next%d\n",code->next->res->value.int_v);
 			break;
 			case ASSIGN_:
 				fprintf(stdout, "ASSIGN %s %s\n",code->arg1->id, code->res->id);
@@ -151,7 +157,7 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					break;
 					case FLOAT_:
 					//fprintf(output, "\tla $a0, %s\n",code->arg1->id);
-					fprintf(output, "\tl.s $f0, %s\n",code->arg1->id);
+					fprintf(output, "\tl.s $f0, %s\n", code->arg1->id);
 					fprintf(output, "\ts.s $f0, %s\n", code->res->id);
 					break;
 					default:break;
@@ -161,20 +167,20 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 				{
 					//Symbol *s = symbol_lookup(&symbol_table, code->res->id);
 					fprintf(stdout, "GOTO %s\n", code->res->id);
-					fprintf(output, "\tj next%d\n",code->next->label);
+					fprintf(output, "\tj next%d\n", code->next->label);
 				}
 			break;
-			/*case NOP_:
+			case NOP_:
 				fprintf(output, "\t#NOP\n");
 				fprintf(output, "\tnop\n");
-			break;*/
+			break;
 			case PRINTF_:
 			case PRINT_:
 				switch(code->arg1->type){
 					case INT_:
 					fprintf(output, "\tli $v0, 1\n");
-					fprintf(output, "\tsw $t2, %s\n", code->arg1->id);
-					fprintf(output, "\tmove $a0, $t2\n");
+					fprintf(output, "\tlw $t0, %s\n", code->arg1->id);
+					fprintf(output, "\tmove $a0, $t0\n");
 					break;
 					case FLOAT_:
 					fprintf(output, "\tli $v0, 2\n");
@@ -190,11 +196,17 @@ int translate_to_mips(FILE *output, Symbol* symbol_table, Quad* code) {
 					fprintf(output, "\tli $v0, 4\n");
 					fprintf(output, "\tla $a0, \"null\"\n");
 					break;
+					default:break;
 				}
 				fprintf(output, "\tsyscall\n");
+				if(code->next != NULL && code->next->res != NULL  && code->next->res->isConstant && code->next->res->value.int_v > 0)
+					fprintf(output, "\tj next%d\n",code->next->res->value.int_v);
 			break;
 		}
+		if(code->res != NULL  && code->res->value.int_v > 0)
+			fprintf(output, "\tj next%d\n",code->res->value.int_v);
 		code = code->next;
 	}
+	fprintf(output, "\n\tjr $ra\n");
 	return 0;
 }
